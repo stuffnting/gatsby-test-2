58ff5e821f216f3f4858f92678896e655a4782f7
Grover Stones <richard@stuffnting.com>
2022-10-01 14:03:48 +0100

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,9 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Test 2`,
     siteUrl: `https://www.yourdomain.tld`,
+    description: `My second Gatsby tester`,
+    twitterUsername: `@gatsbyjs`,
+    image: `/img/gatsby-icon.png`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,26 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { SearchEngOpt } from "../components/seo";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+      <StaticImage alt="A pooch sat on grass." src="../images/pooch.jpeg" />
     </Layout>
   );
 };
 
-export const Head = () => <title>Home Page</title>;
+export const Head = () => (
+  <>
+    <SearchEngOpt />
+    <title>Home Page</title>
+  </>
+);
 
 export default IndexPage;
